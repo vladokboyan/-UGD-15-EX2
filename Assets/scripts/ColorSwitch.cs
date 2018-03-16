af07263fcf2183f0c78333c1e36c6fb91ae19408
vladokboyan <vladokboyan@gmail.com>
2018-03-16 20:39:32 +0200

    Normal part 2

---
+++ Assets/scripts/ColorSwitch.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorSwitch : MonoBehaviour
+{
+
+    Material m_Material;
+
+    void Start()
+    {
+        m_Material = GetComponent<Renderer>().material;
+        m_Material.color = Color.magenta;
+    }
+
+}
